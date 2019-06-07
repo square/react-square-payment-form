e096770d46a73208cf37d29b89badf27a5b71a44
Stephanie Taylor <staylor@squareup.com>
2019-06-07 13:58:06 -0700

    Add linter config

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parserOptions: {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "useJSXTextNode": true,
+    "project": "./tsconfig.json",
+    "tsconfigRootDir": ".",
+  },
+  rules: {
+    "react/prop-types": "off",
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/explicit-function-return-type": [
+      "warn",
+      {
+        "allowExpressions": true,
+        "allowTypedFunctionExpressions": true
+      }
+    ]
+  },
+};
