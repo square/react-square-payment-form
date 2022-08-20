431efe409caa06f72184d34d9975ac0dcdefb845
Max Beatty <mbeatty@squareup.com>
2022-08-19 19:02:03 -0700

    SqPaymentForm is deprecated

--- postInstall.js
+++ postInstall.js
@@ -1,3 +1,5 @@
-const colors = require('colors');
-
-console.log(colors.red('On 2022-07-15, SqPaymentForm will be retired and functionality will no longer work. We encourage you to upgrade to the Web Payments SDK \n > https://developer.squareup.com/docs/web-payments/overview'));
+console.log(
+  require('colors').red(
+    "SqPaymentForm will no longer work after October 31, 2022. Please migrate to Square's Web Payments SDK https://developer.squareup.com/docs/web-payments/migrate"
+  )
+);
